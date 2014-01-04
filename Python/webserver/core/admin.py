7324d991f654487d3ea002a74fbd07516b74f5d1
Wojtek Siudzinski <admin@suda.pl>
2014-01-04 13:41:50 +0100

    Improved admin

--- Python/webserver/core/admin.py
+++ Python/webserver/core/admin.py
@@ -1,23 +1,33 @@
 from django.contrib import admin
+
 from .models import *
+from .tasks import send_command
 
 class VendorAdmin(admin.ModelAdmin):
     pass
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'vendor', ]
+    list_filter = ('vendor', )
 
 class GroupAdmin(admin.ModelAdmin):
     pass
 
 class DeviceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'product', 'group', 'state']
+    list_filter = ('group', 'product', 'state', )
+
+def send_selected(modeladmin, request, queryset):
+    for item in queryset:
+        send_command.delay(item.pk)
+send_selected.short_description = "Send selected commands"
 
 class CommandAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['device', 'added_on', 'sent_on', 'kind']
+    actions = [send_selected]
 
 class UpdateAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['device', 'received_on', 'kind']
 
 admin.site.register(Vendor, VendorAdmin)
 admin.site.register(Product, ProductAdmin)
